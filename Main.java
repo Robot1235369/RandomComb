672de0038ab283ecd324b73130a48b1d4899d0db
Robot1235369 <IsaacCA2018@gmail.com>
2022-04-19 00:00:57 +0000

    stuff

--- Main.java
+++ Main.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         while(true) {
-            
+            Scanner scan = new Scanner(System.in);
+            String exit = scan.next();
+            if (exit.equals("exit")) {
+                break;
+            }
             Random rand = new Random();
             int num = rand.nextInt(8300);
             num += 1700;
